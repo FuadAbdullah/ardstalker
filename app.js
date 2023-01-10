fbfed617c05c109650f8d357959c58418ef6afc0
FuadAbdullah <muhammadfuadbinabdullah@gmail.com>
2023-01-10 18:52:52 +0800

    Refined the code to utilize edited message listener for live map update

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const yeemunUser = {
   username: process.env.YEEMUN_USERNAME,
 }; // atipoad user id
 
+let myGFLocationMsgID = 0;
+
 // Repeat the text given
 ardstalker.onText(/\/echo (.+)/, (msg, match) => {
   debugMsg({ message: `Echo feature triggered, input = ${match[1]}` });
@@ -25,72 +27,105 @@ ardstalker.onText(/\/echo (.+)/, (msg, match) => {
 });
 
 ardstalker.on("location", (msg) => {
-  notifyMeOfYeeMunLocation(msg);
+  notifyLatestLocation(msg);
 });
 
-const notifyMeOfYeeMunLocation = (msg) => {
-  debugMsg({ message: `UserID: ${msg.chat.id}` });
-  debugMsg({ message: `Username: ${msg.chat.username}` });
-
-  debugMsg({ message: `Location received` });
-  debugMsg({ message: `Latitude: ${msg.location.latitude}` });
-  debugMsg({ message: `Longitude: ${msg.location.longitude}` });
-
-  if (
-    msg.chat.id === yeemunUser.id ||
-    msg.chat.username === yeemunUser.username
-  ) {
-    debugMsg({ message: `User is Yee Mun` });
-    ardstalker.sendMessage(myUser.id, `Yee Mun has sent a location`);
-    ardstalker.sendMessage(
-      myUser.id,
-      `Location details: \n\nLatitude: ${msg.location.latitude} \n\nLongitude: ${msg.location.longitude}`
-    );
-
-    ardstalker.sendLocation(
-      myUser.id,
-      msg.location.latitude,
-      msg.location.longitude,
-      { live_period: 60 }
-    ).then((msg) => {
-        liveLocationUpdatePublisher(msg, true);
-        setTimeout(() => {
-            clearInterval();
-        }, 60000)
-    });
+ardstalker.on("edited_message", (msg) => {
+  // ardstalker.sendMessage(msg.chat.id, "Your message was edited");
+  if (typeof msg.location !== "undefined") {
+    // ardstalker.sendMessage(msg.chat.id, "This is a location");
+    liveLocationUpdatePublisher(myGFLocationMsgID, msg);
   }
-};
-
-const liveLocationUpdatePublisher = (msg, isFromYeeMun = false, timer = 5000) => {
-  debugMsg({
-    message: `Chat ID: ${isFromYeeMun ? myUser.id : msg.chat.id}`,
-  });
-  debugMsg({
-    message: `Message ID: ${msg.message_id}`,
-  });
-  setInterval(
-    () =>
-      ardstalker.editMessageLiveLocation(
-        msg.location.latitude,
-        msg.location.longitude,
-        { chat_id: myUser.id, message_id: msg.message_id }
-      ),
-    timer
-  );
-};
-
-// Continue working on live location dispenser from live message edit
-
-const liveLocationSubscriber = (msg, timer = 5000) => {
-    setInterval(() => {
-
-    }, timer);
-}
+});
 
 ardstalker.on("polling_error", (msg) => {
   debugMsg(msg);
 });
 
+ardstalker.on("error", (msg) => {
+  debugMsg(msg);
+});
+
 const debugMsg = ({ message = "Test Message" }) => {
-  console.log(message);
+  if (process.env.ENVIRONMENT_STATUS !== "PRODUCTION") console.log(message);
+};
+
+const notifyLatestLocation = (msg) => {
+  if (
+    typeof msg.location !== "undefined" &&
+    typeof msg.location.live_period === "undefined"
+  )
+    msg.location.live_period = 0;
+
+  if (
+    msg.chat.id === yeemunUser.id ||
+    msg.chat.username === yeemunUser.username
+  ) {
+    debugMsg({ message: `Yee Mun has shared her location` });
+    debugMsg({
+      message: `Live period is set? ${msg.location.live_period !== 0}`,
+    });
+    locationDetailsMessage(msg, true);
+    sendLocationToUser(
+      msg,
+      true,
+      msg.location.live_period !== 0,
+      msg.location.live_period
+    );
+  } else {
+    locationDetailsMessage(msg);
+  }
+
+  debugMsg({ message: `UserID: ${msg.chat.id}` });
+  debugMsg({ message: `Username: ${msg.chat.username}` });
+  debugMsg({ message: `Location received` });
+  debugMsg({ message: `Latitude: ${msg.location.latitude}` });
+  debugMsg({ message: `Longitude: ${msg.location.longitude}` });
+};
+
+const locationDetailsMessage = (msg, isMyGF = false) => {
+  const otherUsers = `<strong>${msg.chat.username} of user ID ${msg.chat.id} has shared their location</strong>\n\n<i>Location details:</i>\nLatitude: <u>${msg.location.latitude}</u>\nLongitude: <u>${msg.location.longitude}</u>`;
+  const myGF = `<strong>Yee Mun has shared her location</strong>\n\n<i>Location details:</i>\nLatitude: <u>${msg.location.latitude}</u>\nLongitude: <u>${msg.location.longitude}</u>\nLive Period: <u>${msg.location.live_period} seconds</u>`;
+
+  if (isMyGF)
+    ardstalker.sendMessage(myUser.id, myGF, {
+      parse_mode: "HTML",
+    });
+};
+
+const sendLocationToUser = (
+  msg,
+  isMyGF = false,
+  isLive = false,
+  livePeriod = 60
+) =>
+  ardstalker
+    .sendLocation(
+      isMyGF ? myUser.id : msg.chat.id,
+      msg.location.latitude,
+      msg.location.longitude,
+      {
+        live_period: isLive ? livePeriod : 0,
+      }
+    )
+    .then((msg) => {
+      if (isMyGF) myGFLocationMsgID = msg.message_id;
+    });
+
+const liveLocationUpdatePublisher = (receiverSideMsgID, msg, timer = 900) => {
+  debugMsg({
+    message: `Chat ID: ${msg.chat.id}`,
+  });
+  debugMsg({
+    message: `Sender Message ID: ${msg.message_id}`,
+  });
+  debugMsg({
+    message: `Receiver Side Message ID: ${receiverSideMsgID}`,
+  });
+
+  ardstalker.editMessageLiveLocation(
+    msg.location.latitude,
+    msg.location.longitude,
+    { chat_id: myUser.id, message_id: receiverSideMsgID }
+  );
 };
